e69eb4b8c3e871e8b8e7cb3f51b7a300abb06afb
davenpi <iandavenport@g.harvard.edu>
2022-09-04 15:11:34 -0400

    make kymograph with high frequency sampling ims in the correct order

--- analyze_ims.py
+++ analyze_ims.py
@@ -19,33 +19,50 @@ os.mkdir(
     save_folder,
 )
 
-i = 0
+
+files = []
 for filename in os.listdir(ims_path):
     if filename.endswith(".png"):
         im_file = os.path.join(ims_path, filename)
-        # print("Image path is: " + im_file + "\n")
-        # print("Save path is: " + save_path + "\n")
-        img = vh.load_img(img_path=im_file)
-        front = vh.extract_front(img)
-        distance = vh.get_distance_parameter(outline=front)
-        interpolated_line, interpolator = vh.get_interpolation(
-            distance, front, num_points=15
-        )
-        interpolation_points = vh.get_interp_points(distance=distance, num_points=15)
-        smooth_kappa, smooth_points = vh.compute_curvature(
-            interpolated_line, interpolation_points
-        )
-        # vh.plot_interpolated_line(
-        #     cropped=img, outline=front, interpolated_line=interpolated_line, size=6
-        # )
+        files.append(im_file)
 
-        # I need to do better here on the ordering of the files. idk why
-        # they os thing is looping through in such a weird order.
-        np.savez(
-            save_folder + f"/{i}",
-            img,
-            interpolated_line,
-            smooth_points,
-            smooth_kappa,
-        )
-        i += 1
+# sort the list of files
+
+
+def extract_nums_from_str(string: str) -> int:
+    """
+    Extract numbers from string of the form 'stringxxxx.png'
+    """
+    return int(string[-8:-4])
+
+
+files = sorted(files, key=extract_nums_from_str)
+
+i = 0
+for im_file in files:
+    # print("Image path is: " + im_file + "\n")
+    # print("Save path is: " + save_folder + "\n")
+    img = vh.load_img(img_path=im_file)
+    front = vh.extract_front(img)
+    distance = vh.get_distance_parameter(outline=front)
+    interpolated_line, interpolator = vh.get_interpolation(
+        distance, front, num_points=15
+    )
+    interpolation_points = vh.get_interp_points(distance=distance, num_points=15)
+    smooth_kappa, smooth_points = vh.compute_curvature(
+        interpolated_line, interpolation_points
+    )
+    # vh.plot_interpolated_line(
+    #     cropped=img, outline=front, interpolated_line=interpolated_line, size=6
+    # )
+
+    # I need to do better here on the ordering of the files. idk why
+    # they os thing is looping through in such a weird order.
+    np.savez(
+        save_folder + f"/im{i}",
+        img,
+        interpolated_line,
+        smooth_points,
+        smooth_kappa,
+    )
+    i += 1
